7feebbc06f64e68dce56e4f9a276fa31b254ebe9
khw7876 <khw7876@gmail.com>
2022-09-07 15:30:10 +0900

    feat/ 비밀번호 없이 게시물 수정 구현

--- secured_post/post/urls.py
+++ secured_post/post/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.PostView.as_view()),
+    path("<int:post_id>", views.PostView.as_view()),
 ]
--- secured_post/post/views.py
+++ secured_post/post/views.py
@@ -2,7 +2,10 @@ from rest_framework.views import APIView
 from rest_framework import status, exceptions
 from rest_framework.response import Response
 
-from post.Services.post_service import create_post
+from post.Services.post_service import (
+    create_post,
+    update_post,
+    )
 
 # Create your views here.
 
@@ -11,3 +14,7 @@ class PostView(APIView):
     def post(self, request):
         create_post(request.data)
         return Response({"detail" : "게시글을 작성하였습니다."}, status=status.HTTP_200_OK)
+
+    def put(self, request, post_id):
+        update_post(request.data, post_id)
+        return Response({"detail" : "게시글을 수정하였습니다."}, status=status.HTTP_200_OK)
