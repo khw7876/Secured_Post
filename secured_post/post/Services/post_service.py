3484d7add5a3095b31f1df2a8a0bcc38edc19319
khw7876 <khw7876@gmail.com>
2022-09-07 16:04:50 +0900

    refac/ 비밀번호가 일치하는지 확인하는 service 수정

--- secured_post/post/Services/post_service.py
+++ secured_post/post/Services/post_service.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.hashers import check_password
+
 from post.models import Post as PostModel
 from post.serializers import CreatePostSerializer, PostSerializer
 
@@ -7,13 +9,17 @@ def create_post(create_data):
     post_serializer.save()
 
 def update_post(update_data, post_id):
-    update_post = PostModel.objects.get(id=post_id)
-    post_serializer = PostSerializer(update_post, update_data, partial= True)
+    update_post_obj = PostModel.objects.get(id=post_id)
+    post_serializer = PostSerializer(update_post_obj, update_data, partial= True)
     post_serializer.is_valid(raise_exception=True)
     post_serializer.save()
 
-def check_password(check_data, post_id):
+def check_is_password(check_data, post_id):
     check_post = PostModel.objects.get(id=post_id)
-    if check_data["password"] == check_post.password:
+    if check_password(check_data["password"], check_post.password):
         return True
     return False
+
+def delete_post(post_id):
+    delete_post_obj = PostModel.objects.get(id=post_id)
+    delete_post_obj.delete()
