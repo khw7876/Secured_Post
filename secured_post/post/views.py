890d67625feda43f925d504e0871ded2845331c6
khw7876 <khw7876@gmail.com>
2022-09-07 16:05:02 +0900

    feat/ delete_post 구현

--- secured_post/post/views.py
+++ secured_post/post/views.py
@@ -5,7 +5,8 @@ from rest_framework.response import Response
 from post.Services.post_service import (
     create_post,
     update_post,
-    check_password,
+    check_is_password,
+    delete_post,
     )
 
 # Create your views here.
@@ -17,7 +18,14 @@ class PostView(APIView):
         return Response({"detail" : "게시글을 작성하였습니다."}, status=status.HTTP_200_OK)
 
     def put(self, request, post_id):
-        if check_password(request.data, post_id):
+        if check_is_password(request.data, post_id):
             update_post(request.data, post_id)
             return Response({"detail" : "게시글을 수정하였습니다."}, status=status.HTTP_200_OK)
         return Response({"detail" : "비밀번호가 일치하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, post_id):
+        if check_is_password(request.data, post_id):
+            delete_post(post_id)
+            return Response({ "detail" : "게시글이 삭제 되었습니다."}, status=status.HTTP_200_OK)
+        return Response({"detail" : "비밀번호가 일치하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+        
